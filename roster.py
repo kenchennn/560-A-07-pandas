bd9359efdc7554ef862eb8581a8643fe2ee1bde3
kenchennn <158109656+kenchennn@users.noreply.github.com>
2024-05-09 02:25:40 -0400

    5

--- roster.py
+++ roster.py
@@ -1,10 +1,10 @@
 # link to the roster webpage https://goheels.com/sports/mens-basketball/roster
 import pandas as pd
 
-roster =  ["Okonkwo", "Ingram", "Farris"]
-player = {"Last Name": roster,
-         "First Name" : ["James", "Harrison", "Duwe"],
-         "Height" : [68, 67, 67],
-         "Weight" : [230, 225, 210]}}
+player = {"Last Name" : ["Okonkwo", "Ingram", "Farris"],
+          "First Name" : ["James", "Harrison", "Duwe"],
+          "Height" : [68, 67, 67],
+          "Weight" : [230, 225, 210]
+          }
 data = pd.DataFrame(player)
 print(data)
