e6c6775905a21e92792c2a83dfcf42e6e2ce8252
kenchennn <158109656+kenchennn@users.noreply.github.com>
2024-05-09 02:01:30 -0400

    Update roster.py

--- roster.py
+++ roster.py
@@ -1,16 +1,1 @@
-# link to the roster webpage https://goheels.com/sports/mens-basketball/roster
-import pandas as pd
-
-player = {"Last Name" : ["Okonkwo", "Ingram", "Duwe"],
-            "First Name" : ["James", "Harrison", "Farris"],
-            "Height" : [68, 67, 67],
-            "Weight" : [230, 225, 210]}
-
-data = pd.DataFrame(player)
-
-#bmi
-data["bmi"] = ((data["Weight"])/(data["Height"])**2) * 703
-
-print(data)
-
-data.to_csv("bmi.csv")
+['James', 'Duwe', 'Farris', 'Rob', 'Jalen']
