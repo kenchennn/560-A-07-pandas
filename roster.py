f090b3c7cc3106bc31373f5be9f11b89f2fcd575
kenchennn <158109656+kenchennn@users.noreply.github.com>
2024-05-09 02:17:08 -0400

    roster as loop

--- roster.py
+++ roster.py
@@ -1,1 +1,4 @@
-['James', 'Duwe', 'Farris', 'Rob', 'Jalen']
+# link to the roster webpage https://goheels.com/sports/mens-basketball/roster
+
+roster =  ["Okonkwo", "Ingram", "Farris"]
+print(roster)
